0df97f2867f17d151f3061d6278972734c0af5ca
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-11-25 12:42:01 -0300

    Fixed some problems with productoeditar

--- primer-proyecto/resources/views/categoriaeditar.blade.php
+++ primer-proyecto/resources/views/categoriaeditar.blade.php
@@ -8,17 +8,17 @@
         <form action="/categoriaactualizar" method="post">
             @csrf
             <div class="input-group mb-3">
-            <input type="text" name="id" id="id" style="display:none" value="{{$categoria[0]->id}}">
-            <input type="text" name="nombre" class="form-control" placeholder="Nombre" value="{{$categoria[0]->nombre}}">
-            <span class="input-group-text" id="basic-addon2">Nombre Categoría</span>
+                <input type="text" name="id" id="id" style="display:none" value="{{$categoria[0]->id}}">
+                <input type="text" name="nombre" class="form-control" placeholder="Nombre" value="{{$categoria[0]->nombre}}">
+                <span class="input-group-text" id="basic-addon2">Nombre Categoría</span>
             </div>
             <div class="input-group mb-3">
-            <input type="text" name="descripcion" class="form-control" placeholder="Descripción de la categoría" value="{{$categoria[0]->descripcion}}">
-            <span class="input-group-text" id="basic-addon2">Descripción</span>
+                <input type="text" name="descripcion" class="form-control" placeholder="Descripción de la categoría" value="{{$categoria[0]->descripcion}}">
+                <span class="input-group-text" id="basic-addon2">Descripción</span>
             </div>
             <input class="btn btn-primary" type="submit" value="Guardar Cambios">
-        </form>   
+        </form>
     </div>
-    
+
 </div>
 @endsection
